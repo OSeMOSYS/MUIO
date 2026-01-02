d39ef71d40d108bec83f4d67fbe96ad913829544
june <jka358@sfu.ca>
2026-01-02 13:00:44 -0800

    UNIX system env

--- API/Classes/Case/ImportTemplate.py
+++ API/Classes/Case/ImportTemplate.py
@@ -896,6 +896,7 @@ class ImportTemplate():
                                     # if key not in xlsObject:
                                     #     xlsObject[key] = self.refR(xlsArray)
                                     xlsObject[key] = self.refR(xlsArray)
+                                    print(xlsArray)
                                     jsonData[a['id']]['SC_0'][0]['value'] = xlsObject[key]['RE1']
 
                                 if key == 'RT':
